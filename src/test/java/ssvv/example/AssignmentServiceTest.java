54b6367d7b0ca420dd8a18a816fb875b0b94a363
unknown <lavinia.raduta1@gmail.com>
2022-04-04 15:39:52 +0300

    finish lab3 - take home

--- src/test/java/ssvv/example/AssignmentServiceTest.java
+++ src/test/java/ssvv/example/AssignmentServiceTest.java
@@ -1,12 +1,13 @@
-package ssvv.example.service;
+package ssvv.example;
 
 import org.junit.Test;
 import ssvv.example.repository.TemaXMLRepository;
+import ssvv.example.service.Service;
 import ssvv.example.validation.TemaValidator;
 
 import static org.junit.Assert.assertEquals;
 
-public class AssignmentTest {
+public class AssignmentServiceTest {
     private final TemaValidator temaValidator = new TemaValidator();
     private final TemaXMLRepository temaXMLRepository =
             new TemaXMLRepository(temaValidator, "assignments.xml");
@@ -14,19 +15,19 @@ public class AssignmentTest {
 
     @Test
     public void addAssignment_ValidInput_AssignmentAddedSuccessfully() {
-        assertEquals(1, service.saveTema("1", "aaa", 7, 5));
-        service.deleteTema("1");
-    }
-
-    @Test
-    public void addAssignment_AlreadyExistingAssignment_AssignmentNotAdded() {
-        service.saveTema("1", "aaa", 7, 5);
-        assertEquals(0, service.saveTema("1", "aaa", 7, 5));
+        assertEquals(1, service.saveTema("1", "descriere", 3, 1));
         service.deleteTema("1");
     }
 
     @Test
     public void addAssignment_InvalidDeadlineInput_AssignmentNotAdded() {
-        assertEquals(0, service.saveTema("1", "aaa", 20, 5));
+        assertEquals(0, service.saveTema("1", "descriere", 20, 1));
+    }
+
+    @Test
+    public void addAssignment_AlreadyExistingAssignment_AssignmentNotAdded() {
+        service.saveTema("1", "descriere1", 7, 5);
+        assertEquals(0, service.saveTema("1", "descriere1", 3, 1));
+        service.deleteTema("1");
     }
 }
---
+++ src/test/java/ssvv/example/AssignmentValidatorTest.java
@@ -0,0 +1,160 @@
+package ssvv.example;
+
+import org.junit.Test;
+import ssvv.example.domain.Tema;
+import ssvv.example.validation.TemaValidator;
+import ssvv.example.validation.ValidationException;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AssignmentValidatorTest {
+    private final TemaValidator temaValidator = new TemaValidator();
+
+    @Test
+    public void addAssignment_InvalidId_NullId_ThrowsError() {
+        Tema tema = new Tema(null, "description", 3, 1);
+
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            temaValidator.validate(tema);
+        });
+
+        String expectedMessage = "ID invalid! \n";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void addAssignment_InvalidId_EmptyId_ThrowsError() {
+        Tema tema = new Tema("", "description", 3, 1);
+
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            temaValidator.validate(tema);
+        });
+
+        String expectedMessage = "ID invalid! \n";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void addAssignment_InvalidDescription_NullDescription_ThrowsError() {
+        Tema tema = new Tema("1", null, 3, 1);
+
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            temaValidator.validate(tema);
+        });
+
+        String expectedMessage = "Descriere invalida! \n";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void addAssignment_InvalidDescription_EmptyDescription_ThrowsError() {
+        Tema tema = new Tema("1", "", 3, 1);
+
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            temaValidator.validate(tema);
+        });
+
+        String expectedMessage = "Descriere invalida! \n";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void addAssignment_InvalidDeadline_DeadlineTooLow_ThrowsError() {
+        Tema tema = new Tema("1", "description", 0, 1);
+
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            temaValidator.validate(tema);
+        });
+
+        String expectedMessage = "Deadline invalid! \n";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void addAssignment_InvalidDeadline_DeadlineTooHigh_ThrowsError() {
+        Tema tema = new Tema("1", "description", 15, 1);
+
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            temaValidator.validate(tema);
+        });
+
+        String expectedMessage = "Deadline invalid! \n";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void addAssignment_InvalidDeadline_DeadlineLowerThanStartline_ThrowsError() {
+        Tema tema = new Tema("1", "description", 1, 3);
+
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            temaValidator.validate(tema);
+        });
+
+        String expectedMessage = "Deadline invalid! \n";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void addAssignment_InvalidStartline_StartlineTooLow_ThrowsError() {
+        Tema tema = new Tema("1", "description", 1, 0);
+
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            temaValidator.validate(tema);
+        });
+
+        String expectedMessage = "Data de primire invalida! \n";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    /*@Test
+    public void addAssignment_InvalidStartline_StartlineTooHigh_ThrowsError() {
+        Tema tema = new Tema("1", "description", 1, 15);
+
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            temaValidator.validate(tema);
+        });
+
+        String expectedMessage = "Data de primire invalida! \n";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void addAssignment_InvalidStartline_StartlineHigherThanDeadline_ThrowsError() {
+        Tema tema = new Tema("1", "description", 1, 3);
+
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            temaValidator.validate(tema);
+        });
+
+        String expectedMessage = "Data de primire invalida! \n";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }*/
+
+    @Test
+    public void addAssignment_ValidInput() {
+        Tema tema = new Tema("1", "description", 3, 1);
+        assertDoesNotThrow(() -> temaValidator.validate(tema));
+
+    }
+}
