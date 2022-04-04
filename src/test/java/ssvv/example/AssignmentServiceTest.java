453acb42939638d7dc09ac323637d936a90f513e
unknown <lavinia.raduta1@gmail.com>
2022-04-04 18:49:45 +0300

    kkk

--- src/test/java/ssvv/example/AssignmentServiceTest.java
+++ src/test/java/ssvv/example/AssignmentServiceTest.java
@@ -1,11 +1,9 @@
 package ssvv.example;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import ssvv.example.repository.TemaXMLRepository;
 import ssvv.example.service.Service;
 import ssvv.example.validation.TemaValidator;
-
-import static org.junit.Assert.assertEquals;
 
 public class AssignmentServiceTest {
     private final TemaValidator temaValidator = new TemaValidator();
@@ -13,21 +11,25 @@ public class AssignmentServiceTest {
             new TemaXMLRepository(temaValidator, "assignments.xml");
     private final Service service = new Service(null, temaXMLRepository, null);
 
-    @Test
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
     public void addAssignment_ValidInput_AssignmentAddedSuccessfully() {
-        assertEquals(1, service.saveTema("1", "descriere", 3, 1));
+        Assertions.assertEquals(1, service.saveTema("1", "descriere", 3, 1));
         service.deleteTema("1");
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void addAssignment_InvalidDeadlineInput_AssignmentNotAdded() {
-        assertEquals(0, service.saveTema("1", "descriere", 20, 1));
+        Assertions.assertEquals(0, service.saveTema("1", "descriere", 20, 1));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void addAssignment_AlreadyExistingAssignment_AssignmentNotAdded() {
         service.saveTema("1", "descriere1", 7, 5);
-        assertEquals(0, service.saveTema("1", "descriere1", 3, 1));
+        Assertions.assertEquals(0, service.saveTema("1", "descriere1", 3, 1));
         service.deleteTema("1");
     }
 }
--- src/test/java/ssvv/example/service/ServiceTest.java
+++ src/test/java/ssvv/example/service/ServiceTest.java
@@ -1,11 +1,9 @@
 package ssvv.example.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import ssvv.example.domain.Student;
 import ssvv.example.repository.StudentXMLRepository;
 import ssvv.example.validation.StudentValidator;
-
-import static org.junit.Assert.*;
 
 public class ServiceTest {
     private final StudentValidator studentValidator = new StudentValidator();
@@ -13,92 +11,96 @@ public class ServiceTest {
             new StudentXMLRepository(studentValidator, "students.xml");
     private final Service service = new Service(studentXMLRepository, null, null);
 
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
     // 1, 6, 8 - the id, name and group are valid
-    @Test
+    @org.junit.jupiter.api.Test
     public void addStudent_ValidInput_StudentAddedCorrectly() {
         Student student1 = new Student("1", "Maria", 111);
         Student student2 = new Student("4", "Ana", 937);
-        assertEquals(1, service.saveStudent("1", "Maria", 111));
-        assertEquals(1, service.saveStudent("4", "Ana", 937));
+        Assertions.assertEquals(1, service.saveStudent("1", "Maria", 111));
+        Assertions.assertEquals(1, service.saveStudent("4", "Ana", 937));
 
         boolean found1 = false, found2 = false;
 
         Iterable<Student> students = service.findAllStudents();
         for(Student s: students) {
             if(s.getID().equals(student1.getID())) {
-                assertEquals(student1.getGrupa(), s.getGrupa());
-                assertEquals(student1.getNume(), s.getNume());
+                Assertions.assertEquals(student1.getGrupa(), s.getGrupa());
+                Assertions.assertEquals(student1.getNume(), s.getNume());
 
                 found1 = true;
             }
 
             if(s.getID().equals(student2.getID())) {
-                assertEquals(student2.getGrupa(), s.getGrupa());
-                assertEquals(student2.getNume(), s.getNume());
+                Assertions.assertEquals(student2.getGrupa(), s.getGrupa());
+                Assertions.assertEquals(student2.getNume(), s.getNume());
 
                 found2 = true;
             }
         }
 
-        assertTrue(found1);
-        assertTrue(found2);
+        Assertions.assertTrue(found1);
+        Assertions.assertTrue(found2);
         service.deleteStudent("1");
         service.deleteStudent("4");
     }
 
     // 2 - the group is invalid (lower than 111)
-    @Test
+    @org.junit.jupiter.api.Test
     public void addStudent_InvalidInput_GroupLower_StudentNotAdded() {
-        assertEquals(0, service.saveStudent("1", "Maria", 110));
+        Assertions.assertEquals(0, service.saveStudent("1", "Maria", 110));
     }
 
     // 3 - the group is invalid (higher than 937)
-    @Test
+    @org.junit.jupiter.api.Test
     public void addStudent_InvalidInput_GroupHigher_StudentNotAdded() {
-        assertEquals(0, service.saveStudent("1", "Maria", 938));
+        Assertions.assertEquals(0, service.saveStudent("1", "Maria", 938));
     }
 
     // 7 - the name is invalid (empty string)
-    @Test
+    @org.junit.jupiter.api.Test
     public void addStudent_InvalidInput_EmptyStringName_StudentNotAdded() {
-        assertEquals(0, service.saveStudent("1", "", 123));
+        Assertions.assertEquals(0, service.saveStudent("1", "", 123));
     }
 
     // 7 - the name is invalid (null)
-    @Test
+    @org.junit.jupiter.api.Test
     public void addStudent_InvalidInput_NullName_StudentNotAdded() {
-        assertEquals(0, service.saveStudent("1", null, 123));
+        Assertions.assertEquals(0, service.saveStudent("1", null, 123));
     }
 
     // 9 - the id is invalid (empty string)
-    @Test
+    @org.junit.jupiter.api.Test
     public void addStudent_InvalidInput_EmptyStringId_StudentNotAdded() {
-        assertEquals(0, service.saveStudent("", "Ana", 123));
+        Assertions.assertEquals(0, service.saveStudent("", "Ana", 123));
     }
 
     // 9 - the id is invalid (null)
-    @Test
+    @org.junit.jupiter.api.Test
     public void addStudent_InvalidInput_NullId_StudentNotAdded() {
-        assertEquals(0, service.saveStudent(null, "Ana", 123));
+        Assertions.assertEquals(0, service.saveStudent(null, "Ana", 123));
     }
 
     // 10 - the same id for two different students
-    @Test
+    @org.junit.jupiter.api.Test
     public void addStudentService_InvalidInput_AlreadyExistingID_CorrectLength() {
         service.saveStudent("3", "Maria", 936);
 
-        assertEquals(0, service.saveStudent("3", "Ioana", 123));
+        Assertions.assertEquals(0, service.saveStudent("3", "Ioana", 123));
 
         int length = 0;
         for(Student ignored : service.findAllStudents()) {
             length++;
         }
 
-        assertEquals(1, length);
+        Assertions.assertEquals(1, length);
         service.deleteStudent("3");
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void ValidInput_CorrectElement_AddStudent() {
         Student student = new Student("2", "Maria", 936);
         service.saveStudent("2", "Maria", 936);
@@ -108,14 +110,14 @@ public class ServiceTest {
         Iterable<Student> students = service.findAllStudents();
         for(Student s: students) {
             if(s.getID().equals(student.getID())) {
-                assertEquals(student.getNume(), s.getNume());
-                assertEquals(student.getGrupa(), s.getGrupa());
+                Assertions.assertEquals(student.getNume(), s.getNume());
+                Assertions.assertEquals(student.getGrupa(), s.getGrupa());
 
                 found = true;
             }
         }
 
-        assertTrue(found);
+        Assertions.assertTrue(found);
         service.deleteStudent("2");
     }
 }
