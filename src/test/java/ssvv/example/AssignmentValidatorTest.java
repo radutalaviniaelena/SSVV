0ee60de4901c8f7d08301ebee764f8e27f4074fc
unknown <lavinia.raduta1@gmail.com>
2022-04-04 18:35:14 +0300

    aa

--- src/test/java/ssvv/example/AssignmentValidatorTest.java
+++ src/test/java/ssvv/example/AssignmentValidatorTest.java
@@ -1,19 +1,24 @@
 package ssvv.example;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import ssvv.example.domain.Tema;
 import ssvv.example.validation.TemaValidator;
 import ssvv.example.validation.ValidationException;
 
 import static org.junit.Assert.assertTrue;
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AssignmentValidatorTest {
     private final TemaValidator temaValidator = new TemaValidator();
 
-    @Test
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
     public void addAssignment_InvalidId_NullId_ThrowsError() {
-        /*Tema tema = new Tema(null, "description", 3, 1);
+        Tema tema = new Tema(null, "description", 3, 1);
 
         Exception exception = assertThrows(ValidationException.class, () -> {
             temaValidator.validate(tema);
@@ -22,12 +27,12 @@ public class AssignmentValidatorTest {
         String expectedMessage = "ID invalid! \n";
         String actualMessage = exception.getMessage();
 
-        assertTrue(actualMessage.contains(expectedMessage));*/
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void addAssignment_InvalidId_EmptyId_ThrowsError() {
-       /* Tema tema = new Tema("", "description", 3, 1);
+        Tema tema = new Tema("", "description", 3, 1);
 
         Exception exception = assertThrows(ValidationException.class, () -> {
             temaValidator.validate(tema);
@@ -36,12 +41,12 @@ public class AssignmentValidatorTest {
         String expectedMessage = "ID invalid! \n";
         String actualMessage = exception.getMessage();
 
-        assertTrue(actualMessage.contains(expectedMessage));*/
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void addAssignment_InvalidDescription_NullDescription_ThrowsError() {
-        /*Tema tema = new Tema("1", null, 3, 1);
+        Tema tema = new Tema("1", null, 3, 1);
 
         Exception exception = assertThrows(ValidationException.class, () -> {
             temaValidator.validate(tema);
@@ -50,12 +55,12 @@ public class AssignmentValidatorTest {
         String expectedMessage = "Descriere invalida! \n";
         String actualMessage = exception.getMessage();
 
-        assertTrue(actualMessage.contains(expectedMessage));*/
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void addAssignment_InvalidDescription_EmptyDescription_ThrowsError() {
-       /* Tema tema = new Tema("1", "", 3, 1);
+        Tema tema = new Tema("1", "", 3, 1);
 
         Exception exception = assertThrows(ValidationException.class, () -> {
             temaValidator.validate(tema);
@@ -64,12 +69,12 @@ public class AssignmentValidatorTest {
         String expectedMessage = "Descriere invalida! \n";
         String actualMessage = exception.getMessage();
 
-        assertTrue(actualMessage.contains(expectedMessage));*/
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void addAssignment_InvalidDeadline_DeadlineTooLow_ThrowsError() {
-       /* Tema tema = new Tema("1", "description", 0, 1);
+        Tema tema = new Tema("1", "description", 0, 1);
 
         Exception exception = assertThrows(ValidationException.class, () -> {
             temaValidator.validate(tema);
@@ -78,12 +83,12 @@ public class AssignmentValidatorTest {
         String expectedMessage = "Deadline invalid! \n";
         String actualMessage = exception.getMessage();
 
-        assertTrue(actualMessage.contains(expectedMessage));*/
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void addAssignment_InvalidDeadline_DeadlineTooHigh_ThrowsError() {
-        /*Tema tema = new Tema("1", "description", 15, 1);
+        Tema tema = new Tema("1", "description", 15, 1);
 
         Exception exception = assertThrows(ValidationException.class, () -> {
             temaValidator.validate(tema);
@@ -92,12 +97,12 @@ public class AssignmentValidatorTest {
         String expectedMessage = "Deadline invalid! \n";
         String actualMessage = exception.getMessage();
 
-        assertTrue(actualMessage.contains(expectedMessage));*/
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void addAssignment_InvalidDeadline_DeadlineLowerThanStartline_ThrowsError() {
-        /*Tema tema = new Tema("1", "description", 1, 3);
+        Tema tema = new Tema("1", "description", 1, 3);
 
         Exception exception = assertThrows(ValidationException.class, () -> {
             temaValidator.validate(tema);
@@ -106,12 +111,12 @@ public class AssignmentValidatorTest {
         String expectedMessage = "Deadline invalid! \n";
         String actualMessage = exception.getMessage();
 
-        assertTrue(actualMessage.contains(expectedMessage));*/
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void addAssignment_InvalidStartline_StartlineTooLow_ThrowsError() {
-     /*   Tema tema = new Tema("1", "description", 1, 0);
+        Tema tema = new Tema("1", "description", 1, 0);
 
         Exception exception = assertThrows(ValidationException.class, () -> {
             temaValidator.validate(tema);
@@ -120,7 +125,7 @@ public class AssignmentValidatorTest {
         String expectedMessage = "Data de primire invalida! \n";
         String actualMessage = exception.getMessage();
 
-        assertTrue(actualMessage.contains(expectedMessage));*/
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
     }
 
     /*@Test
@@ -151,10 +156,10 @@ public class AssignmentValidatorTest {
         assertTrue(actualMessage.contains(expectedMessage));
     }*/
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void addAssignment_ValidInput() {
-        /*Tema tema = new Tema("1", "description", 3, 1);
-        assertDoesNotThrow(() -> temaValidator.validate(tema));*/
+        Tema tema = new Tema("1", "description", 3, 1);
+        assertDoesNotThrow(() -> temaValidator.validate(tema));
 
     }
 }
